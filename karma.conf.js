ba002773ee4d1d185c5c4c639cff763ce3ab201a
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2019-02-14 21:43:13 +0000

    Attempt to fix Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,21 @@
 module.exports = function(config) {
+    var chromeOpts = {
+        base: "Chrome"
+    }
+
+    if (process.env.TRAVIS) {
+        chromeOpts = {
+            base: "ChromeHeadless",
+            flags: ["--no-sandbox"]
+        };
+    }
+
     config.set({
         logLevel: config.LOG_INFO,
         frameworks: ['jasmine'],
+        customLaunchers: {
+            ChromeMaybeHeadless: chromeOpts
+        },
         files: [
             "smallquery.js",
             "spec/**/*spec.js"
