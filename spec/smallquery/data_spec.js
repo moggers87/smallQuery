5b294103d60758cd09942a97d5f4dea3909a8644
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2019-02-15 11:45:23 +0000

    Data API
    I've opted to imitate jQuery's method of storing data in an actual
    object rather than on the HTML tag. This means objects (i.e. instance of
    smallQuery or whatever) can be stored via this API too.
    
    I'm tempted to switch to just a basic wrapper around Element.dataset,
    but I'm unsure right now.

--- spec/smallquery/data_spec.js
+++ spec/smallquery/data_spec.js
@@ -15,11 +15,13 @@ describe("the data API", function() {
         });
 
         it("should create a new data attribute", function() {
-            $obj.data("another-test", "flux by baths");
-            expect($obj.data("another-test")).toEqual("flxu by baths");
+            $obj.data("another-test", "voltaic");
+            expect($obj.data("another-test")).toEqual("voltaic");
         });
 
         it("should update an already existing data attribute", function() {
+            $obj.data("test", "yearwounds");
+            expect($obj.data("test")).toEqual("yearwounds");
         });
 
         it("should allow setting the value to arbitrary objects", function() {
@@ -32,15 +34,22 @@ describe("the data API", function() {
     });
 
     describe("with multiple keys", function() {
-        it("should return a dict of key values when not given any args", function() {
-            $obj.data("other", "zZz");
-            expect($obj.data()).toEqual({test: "superego royal jelly", other: "zZz"});
+        beforeEach(function() {
+            $obj.data("other", "lover's lane");
         });
 
-        it("should create a new data attribute", function() {
+        it("should return a dict of key values when not given any args", function() {
+            expect($obj.data()).toEqual({test: "superego royal jelly", other: "lover's lane"});
+        });
+
+        it("should create multiple keys at once", function() {
+            $obj.data({one: 1, two: 2});
+            expect($obj.data()).toEqual({test: "superego royal jelly", other: "lover's lane", one: 1, two: 2});
         });
 
         it("should update an already existing data attribute", function() {
+            $obj.data({test: "yearwounds", other: "goodbye horses"});
+            expect($obj.data()).toEqual({test: "yearwounds", other: "goodbye horses"});
         });
     });
 });
