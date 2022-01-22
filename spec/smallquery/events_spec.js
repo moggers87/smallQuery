41693ed59ff8b23ded61211f621ad0761b2e5dab
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2022-01-22 14:00:55 +0000

    Remove event handlers

--- spec/smallquery/events_spec.js
+++ spec/smallquery/events_spec.js
@@ -19,6 +19,21 @@ describe("the events API", function() {
         expect(spyspy.calls.count()).toBe(1);
     });
 
+    it("should be able to have multiple handlers for the same event type", function() {
+        var spyspy = jasmine.createSpy("spyspy");
+        var callback = function() {
+            spyspy();
+        };
+        var callback2 = function() {
+            spyspy();
+        };
+        $obj.on("click", callback);
+        $obj.on("click", callback2);
+
+        $obj[0].dispatchEvent(new Event("click"));
+        expect(spyspy.calls.count()).toBe(2);
+    });
+
     it("should trigger events on an element", function() {
         var spyspy = jasmine.createSpy("spyspy");
         var callback = function() {
@@ -30,6 +45,52 @@ describe("the events API", function() {
         expect(spyspy.calls.count()).toBe(1);
     });
 
+    it("should remove all event handlers when off is called without arguments", function() {
+        var spyspy = jasmine.createSpy("spyspy");
+        var callback = function() {
+            spyspy();
+        };
+        $obj.on("click", callback);
+        $obj.on("submit", callback);
+        $obj.off();
+
+        $obj[0].dispatchEvent(new Event("click"));
+        expect(spyspy.calls.count()).toBe(0);
+        $obj[0].dispatchEvent(new Event("submit"));
+        expect(spyspy.calls.count()).toBe(0);
+    });
+
+    it("should remove event handlers of a specific event type when off is called with a string", function() {
+        var spyspy = jasmine.createSpy("spyspy");
+        var callback = function() {
+            spyspy();
+        };
+        $obj.on("click", callback);
+        $obj.on("submit", callback);
+        $obj.off("click");
+
+        $obj[0].dispatchEvent(new Event("click"));
+        expect(spyspy.calls.count()).toBe(0);
+        $obj[0].dispatchEvent(new Event("submit"));
+        expect(spyspy.calls.count()).toBe(1);
+    });
+
+    it("should remove a specific event handler when off is called with a string and a function", function() {
+        var spyspy = jasmine.createSpy("spyspy");
+        var callback = function() {
+            spyspy();
+        };
+        var callback2 = function() {
+            spyspy();
+        };
+        $obj.on("click", callback);
+        $obj.on("click", callback2);
+        $obj.off("click", callback);
+
+        $obj[0].dispatchEvent(new Event("click"));
+        expect(spyspy.calls.count()).toBe(1);
+    });
+
     it("should return a smallquery object when calling trigger", function() {
         var result = $obj.trigger("click");
         expect(result).toBe($obj);
@@ -37,6 +98,11 @@ describe("the events API", function() {
 
     it("should return a smallquery object when calling on", function() {
         var result = $obj.on("click", function() {});
+        expect(result).toBe($obj);
+    });
+
+    it("should return a smallquery object when calling off", function() {
+        var result = $obj.off();
         expect(result).toBe($obj);
     });
 });
--- src/events.js
+++ src/events.js
@@ -2,7 +2,13 @@ import smallQuery from "./core.js";
 
 smallQuery.prototype.on = function(eventType, fn) {
     return this.each(function() {
-        this.addEventListener(eventType, fn);
+        addHandler(this, eventType, fn);
+    });
+};
+
+smallQuery.prototype.off = function(eventType, fn) {
+    return this.each(function() {
+        removeHandler(this, eventType, fn);
     });
 };
 
@@ -13,3 +19,39 @@ smallQuery.prototype.trigger = function(eventType) {
         this.dispatchEvent(event);
     });
 };
+
+
+var privEventMarker = "smallQueryEvents" + Math.random();
+
+function addHandler(el, type, fn) {
+    el.addEventListener(type, fn);
+    if (!(privEventMarker in el)) {
+        el[privEventMarker] = {};
+    }
+
+    if (!(type in el[privEventMarker])) {
+        el[privEventMarker][type] = new Set();
+    }
+
+    el[privEventMarker][type].add(fn);
+
+}
+
+function removeHandler(el, type, fn) {
+    if (privEventMarker in el) {
+        for (const eventType in el[privEventMarker]) {
+            if (type !== undefined && type !== eventType) {
+                continue;
+            }
+            for (const handler of el[privEventMarker][eventType]) {
+                if (fn !== undefined && fn !== handler) {
+                    continue;
+                }
+                el.removeEventListener(eventType, handler);
+                el[privEventMarker][eventType].delete(handler);
+            }
+        }
+    }
+}
+
+export default smallQuery;
